67b66454bd3768d42c9cb253c62f24e53ed0b99d
pestebogdan <20840804+pestebogdan@users.noreply.github.com>
2020-04-02 14:36:39 +0300

    basic.sh

--- basic.sh
+++ basic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIRNAME="$BUILD_NUMBER-$VARIANT-$(date +"%m-%d-%Y")"
 mkdir $DIRNAME
-echo "$BUILD_NUMBER" >> ./$DIRNAME/status
+echo "$BUILD_NUMBER - added a global variable $BOGDAN" >> ./$DIRNAME/status
 echo "Done"
